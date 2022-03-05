aadd2b293772d50467928dba61d7fc87e6e5372f
Marina Pavlova <marinap.learning@gmail.com>
2022-03-05 22:57:40 +0000

    Add smooth scroll for menu module

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -5,6 +5,8 @@ const menu = () => {
     const menu = document.querySelector("menu");
     const closeBtn = menu.querySelector(".close-btn");
     const menuItems = menu.querySelectorAll("ul a");
+    const scroll = document.querySelector("main a");
+    console.log(scroll);
 
     const handleMenu = () => {
        menu.classList.toggle("active-menu"); 
@@ -13,7 +15,24 @@ const menu = () => {
     menuBtn.addEventListener("click", handleMenu);
     closeBtn.addEventListener("click", handleMenu);
 
-    menuItems.forEach(item => { item.addEventListener("click", handleMenu); });
+    const smoothScrolling = (element) => {
+        let anchor = element.hash.substring(1);
+        let href = document.getElementById(anchor);
+        href.scrollIntoView({block: "start", behavior: "smooth"});
+    };
+
+    menuItems.forEach(item => { item.addEventListener("click", (event) => {
+        handleMenu();
+        event.preventDefault();
+        smoothScrolling(item);
+        }); 
+    });
+
+    scroll.addEventListener("click", (event) => {
+        event.preventDefault();
+        smoothScrolling(scroll);
+    });
+
 };
 
 export default menu;
