32761f78de2b5f3d4f400070af44ba6ade675db1
Marina Pavlova <marinap.learning@gmail.com>
2022-03-07 19:56:01 +0000

    Add onblur events for inout fields

--- src/modules/validation.js
+++ src/modules/validation.js
@@ -9,10 +9,15 @@ const validation = () => {
     const inputPhone = document.querySelectorAll("form input[type='tel']");
     const inputNumber = document.querySelectorAll(".calc-block input");
 
-    const validationText = /[^а-я^\-^ ]+/i;
-    const validationEmail = /[^a-z^\@^\-^\_^\.^\!^\~^\*^\']+/i;
-    const validationPhone = /[^0-9^\(^\)^\-]+/i;
+    const validationText = /[^а-я\- ]+/i;
+    const validationEmail = /[^a-z\@\-\_\.\!\~\*\']+/i;
+    const validationPhone = /[^0-9\(\)\-]+/i;
     const validationNumber = /[^0-9]+/i;
+
+    const manyDash = /(\-+)/g;
+    const manySpace = /(\s+)/g;
+    const edgeDash = /^\-|\-$/g;
+    const edgeSpace = /^\s+|\s+$/g;
 
     inputText.forEach( input => {
         input.addEventListener("input", (event) => {
@@ -39,6 +44,27 @@ const validation = () => {
     inputNumber.forEach( input => {
         input.addEventListener("input", (event) => {
             event.target.value = event.target.value.replace(validationNumber, "");
+        });
+    });
+
+    inputText.forEach( input => {
+        input.addEventListener("blur", (event) => {
+            event.target.value = event.target.value.replace(manyDash, "-").replace(manySpace, " ").replace(edgeDash, "").replace(edgeSpace, "");
+            let capitalizeLetterFunc = match => match.toUpperCase();
+            event.target.value = event.target.value.toLowerCase();
+            event.target.value = event.target.value.replace(/^[а-я]{1}|\s{1}[а-я]{1}/gi, capitalizeLetterFunc);
+        });
+    });
+
+    inputEmail.forEach( input => {
+        input.addEventListener("blur", (event) => {
+            event.target.value = event.target.value.replace(manyDash, "-").replace(manySpace, " ").replace(edgeDash, "").replace(edgeSpace, "");
+        });
+    });
+
+    inputPhone.forEach( input => {
+        input.addEventListener("blur", (event) => {
+            event.target.value = event.target.value.replace(manyDash, "-").replace(manySpace, " ").replace(edgeDash, "").replace(edgeSpace, "");
         });
     });
 
